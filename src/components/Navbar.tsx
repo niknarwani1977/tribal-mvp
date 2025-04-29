6f59a1d564c9d0f681aef3a556396f21ecd9f172
niknarwani1977 <nikhil77@gmail.com>
2025-04-29 10:20:06 -0400

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,19 +4,13 @@ import { Link } from 'react-router-dom';
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="bg-[#004b6e] fixed bottom-0 w-full z-50 flex justify-around py-3 text-white">
-      <Link to="/login" className="hover:text-gray-300 text-sm">
-        Login
-      </Link>
-      <Link to="/invite" className="hover:text-gray-300 text-sm">
-        Invite
-      </Link>
-      <Link to="/circles" className="hover:text-gray-300 text-sm">
-        Circles
-      </Link>
-      <Link to="/calendar" className="hover:text-gray-300 text-sm">
-        Calendar
-      </Link>
+    <nav className="bg-[#004b6e] fixed bottom-0 w-full z-50 flex justify-around py-3 text-white text-xs sm:text-sm">
+      <Link to="/login" className="hover:text-gray-300">Login</Link>
+      <Link to="/invite" className="hover:text-gray-300">Invite</Link>
+      <Link to="/circles" className="hover:text-gray-300">Circles</Link>
+      <Link to="/calendar" className="hover:text-gray-300">Calendar</Link>
+      <Link to="/create-event" className="hover:text-gray-300">Create Event</Link>
+      <Link to="/notifications" className="hover:text-gray-300">Notifications</Link>
     </nav>
   );
 };
