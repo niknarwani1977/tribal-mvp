0432c7489e689083d41a49f18a2ffbc4bf5d1c7d
niknarwani1977 <nikhil77@gmail.com>
2025-05-23 17:04:16 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { AuthProvider } from './contexts/AuthContext'; // import your AuthProvider
+import { AuthProvider } from './contexts/AuthContext';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <AuthProvider>
-      <App />  {/* Your whole app goes inside AuthProvider */}
-    </AuthProvider>
-  </React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </StrictMode>
 );
