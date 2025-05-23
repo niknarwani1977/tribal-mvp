b68ca50e759c0dd4d94520d1775851cdbb1befa7
niknarwani1977 <nikhil77@gmail.com>
2025-05-23 17:31:56 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,14 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './contexts/AuthContext';
 import './index.css';
 
+// StrictMode is REMOVED here
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
-  </StrictMode>
+  <BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </BrowserRouter>
 );
